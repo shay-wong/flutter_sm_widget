40a889b69c138cfdd969389952a23270305776a4
Shay <369248214@qq.com>
2024-03-15 15:35:22 +0800

    add parameters

--- lib/src/m_scaffold.dart
+++ lib/src/m_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'm_app_bar.dart';
@@ -5,19 +6,40 @@ import 'm_app_bar.dart';
 class MScaffold extends Scaffold {
   MScaffold({
     super.key,
-    super.body,
     PreferredSizeWidget? appBar,
+    super.body,
     String? title,
     Color? titleColor,
     Widget? leading,
     bool isRoot = false,
-    super.backgroundColor,
     List<Widget>? actions,
     VoidCallback? onBack,
     String? previousRoute,
     Color? barBackgourndColor,
     Color? barForegroundColor,
     bool? centerTitle,
+    super.floatingActionButton,
+    super.floatingActionButtonLocation,
+    super.floatingActionButtonAnimator,
+    super.persistentFooterButtons,
+    super.persistentFooterAlignment = AlignmentDirectional.centerEnd,
+    super.drawer,
+    super.onDrawerChanged,
+    super.endDrawer,
+    super.onEndDrawerChanged,
+    super.bottomNavigationBar,
+    super.bottomSheet,
+    super.backgroundColor,
+    super.resizeToAvoidBottomInset,
+    super.primary = true,
+    super.drawerDragStartBehavior = DragStartBehavior.start,
+    super.extendBody = false,
+    super.extendBodyBehindAppBar = false,
+    super.drawerScrimColor,
+    super.drawerEdgeDragWidth,
+    super.drawerEnableOpenDragGesture = true,
+    super.endDrawerEnableOpenDragGesture = true,
+    super.restorationId,
   }) : super(
           appBar: appBar ??
               (title != null || actions != null
