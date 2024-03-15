a71d03a479104b588aa1e9074abcfa082297a515
Shay <369248214@qq.com>
2024-03-15 12:01:41 +0800

    export widget

--- lib/generated/locales.g.dart
+++ lib/generated/locales.g.dart
@@ -2,7 +2,7 @@
 
 // ignore_for_file: lines_longer_than_80_chars, constant_identifier_names
 // ignore: avoid_classes_with_only_static_members
-class MAppTranslation {
+class SMWidgetAppTranslation {
   static Map<String, Map<String, String>> translations = {
     'zh': Locales.zh,
     'en': Locales.en,
@@ -11,11 +11,6 @@ class MAppTranslation {
 
 class LocaleKeys {
   LocaleKeys._();
-  static const reload = 'reload';
-  static const no_data = 'no_data';
-  static const error = 'error';
-  static const no_network = 'no_network';
-  static const state_error_tips = 'state_error_tips';
   static const refresh_complete = 'refresh_complete';
   static const refresh_release_to_load_more = 'refresh_release_to_load_more';
   static const refresh_failed_click_retry = 'refresh_failed_click_retry';
@@ -25,11 +20,6 @@ class LocaleKeys {
 
 class Locales {
   static const zh = {
-    'reload': '重新加载',
-    'no_data': '暂无数据',
-    'error': '错误',
-    'no_network': '当前暂无网络, 请稍后再试',
-    'state_error_tips': '啊哦, 出错了',
     'refresh_complete': '刷新完成',
     'refresh_release_to_load_more': '放开加载更多',
     'refresh_failed_click_retry': '加载失败， 点击重试',
@@ -37,11 +27,6 @@ class Locales {
     'refresh_pull_up_to_load': '上拉加载',
   };
   static const en = {
-    'reload': 'Reload',
-    'no_data': 'No data',
-    'error': 'Error',
-    'no_network': 'There is no network at the moment, please try again later',
-    'state_error_tips': 'A error occurred',
     'refresh_pull_up_to_load': 'Pull-up loading',
     'refresh_release_to_load_more': 'Release to load more',
     'refresh_failed_click_retry': 'Loading failed. Please click to retry.',
--- lib/sm_widget.dart
+++ lib/sm_widget.dart
@@ -2,11 +2,14 @@ library sm_widget;
 
 export 'package:flutter_sm_image/sm_image.dart';
 
+export 'generated/locales.g.dart';
 export 'src/m_app_bar.dart';
 export 'src/m_circle_avatar.dart';
 export 'src/m_container.dart';
 export 'src/m_refresh.dart';
+export 'src/m_rich_text.dart';
 export 'src/m_scaffold.dart';
 export 'src/m_text.dart';
 export 'src/m_text_field.dart';
 export 'src/m_text_span.dart';
+export 'src/text_style_extension.dart';
